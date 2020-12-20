6a4505c6634cbffefdaae3ebbfed8027121f8eaa
pedroivo000 <pedroivo000@gmail.com>
2020-12-20 14:27:36 +0100

    feat: added tests for notes

---
+++ tests/test_note_view.py
@@ -0,0 +1,55 @@
+import pytest
+
+
+def test_get_notes(test_app):
+
+    response = test_app.get("api/v1/notes")
+    record = response.get_json()
+
+    assert response.status_code == 200
+    assert len(record) == 3
+
+
+def test_get_note_by_id(test_app):
+
+    response = test_app.get("api/v1/notes/1")
+    record = response.get_json()
+
+    assert response.status_code == 200
+    assert record["title"] == "test1"
+    assert len(record["content"]) == 2
+    assert record["project_id"] == 1
+    assert record["content"][0]["content"] == "test1"
+
+
+@pytest.mark.parametrize("project_id,expected", [(1, 2), (2, 0)])
+def test_get_notes_by_project(test_app, project_id, expected):
+
+    response = test_app.get("api/v1/notes", query_string={"project_id": project_id})
+    record = response.get_json()
+
+    assert response.status_code == 200
+    assert len(record) == expected
+
+
+@pytest.mark.parametrize("project_id, note_id", [(2, 1), (1, 3)])
+def test_move_note_to_project(test_app, project_id, note_id):
+    payload = {"project_id": project_id}
+
+    response = test_app.put(f"api/v1/notes/{note_id}", json=payload)
+    record = response.get_json()
+
+    assert response.status_code == 200
+    assert record["project_id"] == project_id
+
+
+def test_put_note(test_app):
+    payload = {"title": "new_title", "project_id": 2}
+
+    response = test_app.put("api/v1/notes/1", json=payload)
+    record = response.get_json()
+
+    assert response.status_code == 200
+    assert record["title"] == "new_title"
+    assert record["project_id"] == 2
+    assert record["updated_at"] is not None
